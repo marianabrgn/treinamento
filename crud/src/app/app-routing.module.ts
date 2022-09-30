7a691bd1bf858b8725dc07c5d6a031733d23fa15
marianabrgn <>
2022-09-30 12:46:30 -0300

    crud

---
+++ crud/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
+
+const routes: Routes = [
+  {path:"usuario" , component: UsuarioComponent}  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ crud/src/models/usuario.ts
@@ -0,0 +1,14 @@
+export class Usuario {
+    nome: String | any;
+    cpf: String | any;
+    email: String | any;
+    telefone : String | any;
+    dataNascimento: String | any;
+    estadoCivil: String | any;;
+    cep : String | any;
+    rua : String | any;
+    numero : String | any;
+    bairro : String | any;
+    cidade : String | any;
+    estado : String | any;
+}
